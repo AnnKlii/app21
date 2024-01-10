aeadae05ce4daf55446f6a82151cc48107d05b07
AnnKlii <anna_k84@mail.ru>
2024-01-11 00:06:40 +0300

    add week21

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Chart } from "react-google-charts";
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* const data = [
+      ["Task", "Hours per Day"],
+      ["Work", 11],
+      ["Eat", 2],
+      ["Commute", 2],
+      ["Watch TV", 2],
+      ["Sleep", 7],
+      ]; */}
+
+      {/* const options = [title: "My Daily Activities"]; */}
+
+      <Chart
+        chartType="PieChart"
+        data={[
+          ["Task", "Hours per Day"],
+          ["Work", 11],
+          ["Eat", 2],
+          ["Commute", 2],
+          ["Watch TV", 2],
+          ["Sleep", 7],
+        ]}
+        options={{ title: "My Daily Activities" }}
+        width={"100%"}
+        height={"400px"}
+      />
+
+    </div>
+  );
+}
+
+export default App;
+
